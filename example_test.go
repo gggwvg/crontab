5ceed1db1be8bf4bf3af0c0816404bf20e1e922a
gggwvg <gggwvg@gmail.com>
2020-03-13 14:29:52 +0800

    fix example case

--- example_test.go
+++ example_test.go
@@ -10,17 +10,21 @@ func ExampleScheduleMinute() {
 	m.Add("minute func", "* * * * *", minuteFunc)
 	// 程序退出前可停止调度（可选）
 	m.Stop()
+
+	// Output:
 }
 
 func minuteFunc() {
-	fmt.Println("I'm minuteFunc1")
+	fmt.Println("I'm minuteFunc")
 }
 
 func ExampleScheduleSecond() {
 	s := NewSecond()
-	s.Add("second func", "* */2 * 1 *", secondFunc, "eric", 18)
+	s.Add("second func", "* */2 * 1 * *", secondFunc, "eric", 18)
 	// 不等待调度，立即执行一次所有任务
 	s.Run()
+
+	// Output:
 }
 
 func secondFunc(name string, age int) {
